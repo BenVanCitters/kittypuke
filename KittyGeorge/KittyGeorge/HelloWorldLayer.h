ae6372046da93643cb95b3cf50d915c177817391
Ben Van Citters <van.citters.b@gmail.com>
2014-02-15 15:50:17 -0800

    Initial commit of the auto-generated xcode/ios/cocos2d project

---
+++ KittyGeorge/KittyGeorge/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  KittyGeorge
+//
+//  Created by Ben Van Citters on 2/15/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
